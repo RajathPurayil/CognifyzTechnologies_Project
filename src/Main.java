6ef666180fe1cf9948f496fa9c91ab0d42c4bcff
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-26 02:36:01 -0400

    Updated Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Creating scanner class
+
+
+        Scanner input = new Scanner(System.in);
+
+        // Generate the random number
+
+        int num =(int)(Math.random()*10+1);
+        int numberGuess=3;
+        int Guess;
+
+        // while loop
+
+
+        while (numberGuess>0){
+
+            System.out.println("Guess a number between 1 and  10:");
+            Guess=input.nextInt();
+
+
+            if (Guess==num) {
+
+                System.out.println("Congrats !! The number was  " + num);
+            }else if (Guess<num){
+
+                    System.out.println("Too low.");
+
+            }
+
+            else {
+
+                System.out.println("Too High.");
+
+            }
+
+            numberGuess--;
+
+            if (numberGuess==0){
+
+                System.out.println("You run out of Guess !! Please try again later !! The number was"+num);
+            }
+
+            else {
+
+
+            }
+        }
+
+
+
     }
 }
---
+++ src/README.md
@@ -0,0 +1,16 @@
+             Welcome to Number Guessing Game
+
+                      ### Rules ####
+
+1.The objective of the game is for  player (the guesser) to correct.
+
+2.There may be a limit on the number of guesses the guesser is allowed to make. If the guesser exceeds 3 chance without guessing the correct number, they lose the game.
+
+3.The number must be within the range 1 to 10.
+
+4.The game ends when the guesser correctly guesses the number chosen by the host within the allowed number of guesses. 
+
+
+
+
+
